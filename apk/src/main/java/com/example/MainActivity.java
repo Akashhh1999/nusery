a04408259d6ca4f12091490adeb7027e7c70ff48
Akashhh1999 <130442464+Akashhh1999@users.noreply.github.com>
2024-04-17 06:49:54 -0700

    Add files via upload

---
+++ apk/src/main/java/com/example/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.content.Intent;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        startActivity(new Intent(this, LoginActivity.class));
+    }
+}
